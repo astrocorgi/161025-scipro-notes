ee4daee80ab472af59ae2c3b507579827b8afecf
Cassie Stuurman <cassiestuurman@gmail.com>
2016-10-25 13:17:33 -0500

    first fortran function

---
+++ functionTest.f90
@@ -0,0 +1,21 @@
+program functionTest
+
+integer :: i
+integer, parameter :: n = 200
+real :: x
+real, dimension(n) :: y
+
+x = 0
+i=2;
+x = change(i)
+
+print *, x, i
+
+end program
+
+real function change(j)
+integer :: j
+real :: q
+
+j = 5;
+change = 6;
