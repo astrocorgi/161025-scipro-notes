7814944f50d4eb5c168d354d5fa51e2c97f40463
Cassie Stuurman <cassiestuurman@gmail.com>
2016-10-25 14:58:37 -0500

    Files added
    imported existing class note files

---
+++ sums.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int number1;
+  int number2;
+  int sum;
+
+  cout << "Enter the first integer: ";
+  cin >> number1;
+  cout << "Enter the second integer: ";
+  cin >> number2;
+  sum = number1 + number2;
+  cout << "The sum of two numbbers is: " << sum << endl;
+}
